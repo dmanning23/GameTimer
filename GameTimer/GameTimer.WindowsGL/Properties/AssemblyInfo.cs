972d3e46e3cc7085ec1f7f499e342762346a2c10
Dan Manning <dmanning23@hotmail.com>
2016-03-17 23:24:33 -0500

    gametimer in a nuget package

--- GameTimer/GameTimer.WindowsGL/Properties/AssemblyInfo.cs
+++ GameTimer/GameTimer.WindowsGL/Properties/AssemblyInfo.cs
@@ -5,24 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GameTimer.Windows.XNA")]
-[assembly: AssemblyProduct("GameTimer.Windows.XNA")]
+[assembly: AssemblyTitle("GameTimer.WindowsGL")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2013")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GameTimer.WindowsGL")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("c2eac678-ab74-431c-affc-3ebb5f1f980f")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e84ae3b7-8123-4a26-8225-6a56fdb1c1cf")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,4 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
