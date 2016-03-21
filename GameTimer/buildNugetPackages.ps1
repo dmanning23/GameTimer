85a08f176216f8825f7bb7b42c6b92a421178a44
Dan Manning <dmanning23@hotmail.com>
2016-03-20 21:12:31 -0500

    android nuget packages

---
+++ GameTimer/buildNugetPackages.ps1
@@ -0,0 +1,3 @@
+nuget pack .\GameTimer.Android\GameTimer.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+
+nuget pack .\GameTimer.WindowsGL\GameTimer.WindowsGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
