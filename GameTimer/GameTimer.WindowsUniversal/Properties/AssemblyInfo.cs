16166480603d8e69c844d17f688c490b36605add
Dan Manning <dmanning23@hotmail.com>
2016-04-02 00:01:43 -0500

    windowsgl gone?

--- GameTimer/GameTimer.WindowsUniversal/Properties/AssemblyInfo.cs
+++ GameTimer/GameTimer.WindowsUniversal/Properties/AssemblyInfo.cs
@@ -5,22 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GameTimer.WindowsGL")]
+[assembly: AssemblyTitle("GameTimer.WindowsUniversal")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GameTimer.WindowsGL")]
+[assembly: AssemblyProduct("GameTimer.WindowsUniversal")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e84ae3b7-8123-4a26-8225-6a56fdb1c1cf")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,3 +26,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
