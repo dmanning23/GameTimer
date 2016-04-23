8dd531414fd099e8aac2f629adffc747131b14c0
Dan Manning <dmanning23@hotmail.com>
2016-04-23 13:24:25 -0500

    moved into a pcl

--- GameTimer/buildNugetPackages.ps1
+++ GameTimer/buildNugetPackages.ps1
@@ -1,5 +1,2 @@
-nuget pack .\GameTimer.Android\GameTimer.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\GameTimer.DesktopGL\GameTimer.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\GameTimer.iOS\GameTimer.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\GameTimer.WindowsUniversal\GameTimer.WindowsUniversal.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\GameTimer.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 nuget push *.nupkg
