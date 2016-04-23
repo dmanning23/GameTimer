2dcdc18df8cec59672102dbac3892cad2dd9db75
Dan Manning <dmanning23@hotmail.com>
2016-04-22 22:20:36 -0500

    update build script

--- GameTimer/buildNugetPackages.ps1
+++ GameTimer/buildNugetPackages.ps1
@@ -1,7 +1,5 @@
 nuget pack .\GameTimer.Android\GameTimer.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-
 nuget pack .\GameTimer.DesktopGL\GameTimer.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-
 nuget pack .\GameTimer.iOS\GameTimer.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-
 nuget pack .\GameTimer.WindowsUniversal\GameTimer.WindowsUniversal.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget push *.nupkg
