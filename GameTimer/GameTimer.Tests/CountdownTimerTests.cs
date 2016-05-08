189401e6e871fd931da122feb660d40dbcfa6e45
Dan Manning <dmanning23@hotmail.com>
2016-05-08 14:17:18 -0500

    added a bunch of tests

--- GameTimer/GameTimer.Tests/CountdownTimerTests.cs
+++ GameTimer/GameTimer.Tests/CountdownTimerTests.cs
@@ -1,15 +1,92 @@
+using Microsoft.Xna.Framework;
 using NUnit.Framework;
 using System;
 
 namespace GameTimer.Tests
 {
-	[TestFixture()]
+	[TestFixture]
 	public class CountdownTimerTests
 	{
-		[Test()]
-		public void TestCase()
+		[Test]
+		public void Start_RemainingTime()
 		{
+			var timer = new CountdownTimer();
+			timer.Start(1f);
+			Assert.AreEqual(1, timer.RemainingTime());
+		}
+
+		[Test]
+		public void Start_Lerp()
+		{
+			var timer = new CountdownTimer();
+			timer.Start(1f);
+			Assert.AreEqual(1, timer.Lerp());
+		}
+
+		[Test]
+		public void Half_RemainingTime()
+		{
+			var timer = new CountdownTimer();
+			timer.Start(1f);
+
+			timer.Update(new GameTime(new TimeSpan(0, 0, 0, 0, 500), new TimeSpan(0, 0, 0, 0, 500)));
+
+			Assert.AreEqual(0.5f, timer.RemainingTime());
+		}
+
+		[Test]
+		public void Half_Lerp()
+		{
+			var timer = new CountdownTimer();
+			timer.Start(1f);
+
+			timer.Update(new GameTime(new TimeSpan(0, 0, 0, 0, 500), new TimeSpan(0, 0, 0, 0, 500)));
+
+			Assert.AreEqual(0.5f, timer.Lerp());
+		}
+
+		[Test]
+		public void Quarter_RemainingTime()
+		{
+			var timer = new CountdownTimer();
+			timer.Start(1f);
+
+			timer.Update(new GameTime(new TimeSpan(0, 0, 0, 0, 250), new TimeSpan(0, 0, 0, 0, 250)));
+
+			Assert.AreEqual(0.75f, timer.RemainingTime());
+		}
+
+		[Test]
+		public void Quarter_Lerp()
+		{
+			var timer = new CountdownTimer();
+			timer.Start(1f);
+
+			timer.Update(new GameTime(new TimeSpan(0, 0, 0, 0, 250), new TimeSpan(0, 0, 0, 0, 250)));
+
+			Assert.AreEqual(0.75f, timer.Lerp());
+		}
+
+		[Test]
+		public void Quarter_LotsRemainingTime()
+		{
+			var timer = new CountdownTimer();
+			timer.Start(4f);
+
+			timer.Update(new GameTime(new TimeSpan(0, 0, 0, 1), new TimeSpan(0, 0, 0, 1)));
+
+			Assert.AreEqual(3f, timer.RemainingTime());
+		}
+
+		[Test]
+		public void Quarter_LotsLerp()
+		{
+			var timer = new CountdownTimer();
+			timer.Start(4f);
+
+			timer.Update(new GameTime(new TimeSpan(0, 0, 0, 1), new TimeSpan(0, 0, 0, 1)));
+
+			Assert.AreEqual(0.75f, timer.Lerp());
 		}
 	}
 }
-
