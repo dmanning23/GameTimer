482469de03f5b5d9c2bf2775b6614dc42c4a0600
Dan Manning <dmanning@oceaneering.com>
2021-11-01 17:09:47 -0500

    Added web project

--- GameTimer/buildNugetPackages.ps1
+++ GameTimer/buildNugetPackages.ps1
@@ -1,4 +1,5 @@
 rm *.nupkg
 nuget pack .\GameTimer.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\GameTimer.Bridge.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 cp *.nupkg C:\Projects\Nugets\
 nuget push *.nupkg -Source https://www.nuget.org/api/v2/package
